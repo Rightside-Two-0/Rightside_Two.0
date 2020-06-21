31a093c28bec5c1c565c906916def715f67c3f92
tetrapro <caputo.philip@gmail.com>
2020-06-20 17:34:47 -0700

    updating

---
+++ tests/read_pdf.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# import tabula
+# import pandas as pd
+#
+# df = tabula.read_pdf("testing_this.pdf", pages=6)
+# print(df)
+# # print(df.head())
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+import PyPDF2
+
+pdf_file = open('north_.pdf', 'rb')
+pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+for i in range(pdf_reader.numPages):
+    page = pdf_reader.getPage(i)
+    print(page.extractText())
