efa58e8c47710f366d2f7aee8f1804d2decaf8bd
Philip Caputo Two.0 <philip@two-0.org>
2020-08-04 08:24:17 -0700

    TEST PASS: main window

--- test_rightside_2.py
+++ test_rightside_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-import rightside_2, sys 
-def test_function():
+import rightside_2, sys
+def test___init__():
     app = rightside_2.QtWidgets.QApplication(sys.argv)
     rightside = rightside_2.MainWindow()
     rightside.move(300,750)
     rightside.show()
     app.exec_()
-    assert 1+1+1 == 3
+    assert rightside != None
