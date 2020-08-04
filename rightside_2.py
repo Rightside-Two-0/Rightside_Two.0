eecb13da5a8bb407fe54686fa12ded0a03f836f6
Philip Caputo Two.0 <philip@two-0.org>
2020-08-04 08:52:13 -0700

    TEST PASSED: asset widget

--- rightside_2.py
+++ rightside_2.py
@@ -27,7 +27,105 @@ class ComputeThread(QThread):
         print('testing thread # 21...')
         # git clone done, now inform the main thread with the output
         self.signal.emit('')
+class Asset(QtWidgets.QWidget):
+    def __init__(self):
+        super(Asset, self).__init__()
+        uic.loadUi('guis/asset.ui', self)
+        self.type_in = self.findChild(QtWidgets.QComboBox, 'type_input')
+        self.down_in = self.findChild(QtWidgets.QLineEdit, 'down_input')
+        self.cost_in = self.findChild(QtWidgets.QLineEdit, 'cost_input')
+        self.pymt = self.findChild(QtWidgets.QLineEdit, 'payment_input')
+        self.notes = self.findChild(QtWidgets.QLineEdit, 'notes_input')
+        self.cash_flow_in = self.findChild(QtWidgets.QLineEdit, 'cashflow_input')
+        self.ok_button = self.findChild(QtWidgets.QPushButton, 'ok_button')
+        self.cancel_button = self.findChild(QtWidgets.QPushButton, 'cancel_button')
+        self.ok_button.clicked.connect(self.ok)
+        self.cancel_button.clicked.connect(self.cancel)
+    def ok(self):
+        type = self.type_in.currentText()
+        down = self.down_in.text()
+        cost = self.cost_in.text()
+        pymt= self.pymt.text()
+        note = self.notes.text()
+        cash_flow = self.cash_flow_in.text()
+        mortgage = float(cost) - float(down)
+        #~~~~~~~save~to~~database~~~~~~>
+        try:
+            headers = {"content-type": "application/json"}
+            dict = {
+                "source" : f'{type}',
+                "cost" : f'{cost}',
+                "down" : f'{down}',
+                "notes" : f'{note}'
+            }
+            data = json.dumps(dict)
+            url = 'http://two-0.org:8080/api/assets/'
+            response = requests.post(url, data=data, headers=headers)
+            #~~~~~~update~income~as~well~~~~~>
+            url2 = 'http://two-0.org:8080/api/incomes/'
+            dict2 = {
+                "source": f'{type}',
+                "amount": f'{cash_flow}',
+                "notes": f'{note}',
+            }
+            data2 = json.dumps(dict2)
+            response2 = requests.post(url2, data=data2, headers=headers)
+            #~~~~~~~~~~~~~liability~~~~~~~~~~~>
+            #~~~~~~~~>
+            url3 = 'http://two-0.org:8080/api/liabilities/'
+            if type == 'Real Estate':
+                type = 'RE Mortgages'
+            elif type =='Business':
+                type = 'Business Debts'
+            dict3 = {
+                "source" : f'{type}',
+                "amount" : f'{mortgage}',
+                "notes" : f'{note}'
+            }
+            data3 = json.dumps(dict3)
+            response3 = requests.post(url3, data=data3, headers=headers)
+            #~~~~~~~~~~~~~~~~expense~~~~~~~~~~>
+            #~~~~~~~~>
+            id = 0
+            url4 = 'http://two-0.org:8080/api/expenses/'
+            response_get = requests.get(url4).json()
+            prev_amount = 0.0
+            for item in list(response_get):
+                if item['source'] == 'RE Debt Service':
+                     id = item['id']
+                     prev_amount = item['amount']
+            updated = float(pymt) + float(prev_amount)
+            dict4 = {
+                "source" : 'RE Debt Service',
+                "amount" : f'{updated}',
+                "notes": f'{note}'
+            }
+            if id != 0:
+                data4 = json.dumps(dict4)
+                response4 = requests.put(url4+str(id), data=data4, headers=headers)
+            #~~~~~~~~~~~~why~not~just~get(url)~~~~~>
+            #~~~~~~~~~>I think this is not best...
+            #should call post api then get all
+            window.reload_income()
+            window.reload_expenses()
+            window.reload_assets()
+            window.reload_liabilities()
+            window.update_display()
+        except Exception:
+            traceback.print_exc()
+        #~~~~~~~~~~clean~up~fields~~~~~>
+        down = self.down_in.setText('')
+        cost = self.cost_in.setText('')
+        cash_flow = self.cash_flow_in.setText('')
+        self.hide()
+    def cancel(self):
+        self.hide()
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         uic.loadUi('guis/financial.ui', self)
+        self.add_asset.clicked.connect(self.addAsset)
+    def addAsset(self):
+        self.asset = Asset()
+        self.asset.move(675,150)
+        self.asset.show()
--- test_rightside_2.py
+++ test_rightside_2.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 import rightside_2, sys
-def test___init__():
+# def test___init__():
+#     '''testing main window'''
+#     app = rightside_2.QtWidgets.QApplication(sys.argv)
+#     rightside = rightside_2.MainWindow()
+#     rightside.move(300,750)
+#     rightside.show()
+#     app.exec_()
+#     assert rightside != None
+def test_addAsset():
+    '''testing asset widget'''
     app = rightside_2.QtWidgets.QApplication(sys.argv)
     rightside = rightside_2.MainWindow()
     rightside.move(300,750)
+    widget = rightside_2.Asset()
     rightside.show()
     app.exec_()
-    assert rightside != None
+    assert widget != None
