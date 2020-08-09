133213752dd189aeff65fd91e88d4f1838107042
Philip Caputo Two.0 <philip@two-0.org>
2020-08-09 14:15:34 -0700

    working PyPDF2 & camelot libraries

--- tests/read_pdf.py
+++ tests/read_pdf.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
-# import tabula
-# import pandas as pd
-#
-# df = tabula.read_pdf("testing_this.pdf", pages=6)
-# print(df)
-# # print(df.head())
+import camelot
+import pandas as pd
 
+file = 'document.pdf'
+tables = camelot.read_pdf(file)
+print("Total tables extracted:", tables.n)
+print('Number of tables:', tables.n)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # https://medium.com/@umerfarooq_26378/python-for-pdf-ef0fac2808b0
-import PyPDF2
-
-pdf_file = open('north_.pdf', 'rb')
-pdf_reader = PyPDF2.PdfFileReader(pdf_file)
-for i in range(pdf_reader.numPages):
-    page = pdf_reader.getPage(i)
-    print(page.extractText())
+# import PyPDF2
+#
+# pdf_file = open('north_.pdf', 'rb')
+# pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+# for i in range(pdf_reader.numPages):
+#     page = pdf_reader.getPage(i)
+#     print(page.extractText())
